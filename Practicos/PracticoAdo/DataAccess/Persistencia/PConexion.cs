b87030f8a21103ddafa69c772edd65424088e24a
Ciosek19 <63324847+Ciosek19@users.noreply.github.com>
2025-05-15 21:29:45 -0300

    Creando el repositorio

---
+++ Practicos/PracticoAdo/DataAccess/Persistencia/PConexion.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace DataAccess.Persistencia
+{
+    public class PConexion
+    {
+        private string connectionString = "Server=C01\\SQLEXPRESS;Database=Almacen;User Id=Rodrigo;Password=rodrigo"; //Cambiar el connection string
+        protected SqlConnection GetConnection() 
+        {
+            SqlConnection context = new SqlConnection(connectionString);
+            return context;
+        }
+    }
+}
---
+++ Practicos/PracticoAdo/DataAccess/Repository/ProductoRepository.cs
@@ -0,0 +1,82 @@
+﻿using DataAccess.Persistencia;
+using Models.DTO;
+using System;
+using System.Data.SqlClient;
+
+namespace DataAccess.Repository
+{
+    public class ProductoRepository : PConexion
+    {
+        public void AgregarProducto(Producto producto)
+        {
+            SqlConnection conexion = null;
+            SqlCommand cmd = null;
+
+            using (conexion = this.GetConnection())
+            {
+                try
+                {
+                    conexion.Open();
+                    string sqlAgregarFamilia = "INSERT INTO Producto (CodigoProducto, Descripcion, PrecioVenta, CodigoFamilia) values (@codigo, @descripcion, @precioVenta, @codigoFamilia)";
+                    cmd = new SqlCommand(sqlAgregarFamilia, conexion);
+
+                    cmd.Parameters.AddWithValue("codigo", producto.CodigoProducto);
+                    cmd.Parameters.AddWithValue("descripcion", producto.Descripcion);
+                    cmd.Parameters.AddWithValue("precioVenta", producto.PrecioVenta);
+                    cmd.Parameters.AddWithValue("codigoFamilia", producto.CodigoFamilia);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    if (cmd != null)
+                    {
+                        cmd.Dispose();
+                    }
+                    conexion.Close();
+                }
+            }
+        }
+
+        public int? GetCantidadProductos()
+        {
+            SqlConnection conexion = null;
+            SqlCommand cmd = null;
+            int? cantidad = null;
+
+            using (conexion = this.GetConnection())
+            {
+                try
+                {
+                    conexion.Open();
+                    string sql = "SELECT count(CodigoProducto) as Cantidad FROM Producto";
+                    cmd = new SqlCommand(sql, conexion);
+                    cantidad = (int)cmd.ExecuteScalar();
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    if (cmd != null)
+                    {
+                        cmd.Dispose();
+                    }
+                }
+            }
+            return cantidad;
+        }
+
+        public bool ExisteProducto(int id)
+        {
+            SqlConnection conexion = null;
+            SqlCommand cmd = null
+            
+        }
+    }
+}
